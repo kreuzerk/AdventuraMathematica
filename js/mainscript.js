de1453612b89bce8970b2092d126bfa2539b8fdf
Kevin Kreuzer <kreuzerk@localhost.localdomain>
2014-05-13 08:17:00 +0200

    erstes zeichnen des Karakters

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,4 +1,36 @@
+var currentfield = 1;
+
 function hallo(){
-var random = Math.floor(Math.random() * 6) + 1;
-alert(random);
+
+dicesound = new Audio();
+dicesound.src = "../sounds/Diceroll.mp3";
+dicesound.play();
+
+
+
+var diceresult = Math.floor(Math.random() * 6) + 1;
+
+alert(diceresult);
+
+//Set the new currentfield
+alert(currentfield + " + " + diceresult + " = " + (diceresult + currentfield));
+currentfield = diceresult+currentfield;
+
+var cx = circleData[currentfield-1].cx;
+var cy = circleData[currentfield-1].cy;
+
+drawlion(cx, cy);
+
+height = 200;
+width = 200;
+
+}
+
+
+function drawlion($cx, $cy){
+
+svgContainer.append("svg:image")
+      .attr("xlink:href", "./images/Lion.png")
+      .attr("width", 100)
+      .attr("height", 100).attr("x", $cx-50).attr("y",$cy-90);
 }
