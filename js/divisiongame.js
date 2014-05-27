47719aa356986ca801f84ee8bc6e9f8799173095
Kevin Kreuzer <kreuzerk@localhost.localdomain>
2014-05-27 09:43:09 +0200

    Make sure that same result does not exists twice

--- js/divisiongame.js
+++ js/divisiongame.js
@@ -1,9 +1,10 @@
 var divisionrange = 20;
 var numberoflis = 7;
-var solutionarray = new Array(numberoflis);
+var resultarray = new Array(numberoflis);
+
+
 
 function initialisedivisiongame(){
-
 
 for(var i = 1; i<=numberoflis; i++){
 
@@ -15,27 +16,41 @@ var divisor = Math.floor(Math.random() * divisionrange) + 1;
 var divident = Math.floor(Math.random() * divisionrange) + 1;
 
 if(divisor % divident == 0){
-moduloiszero = true;
+var alreadyexists = false;
+
+//Make sure that the result of this devision does not already exists
+for(var j = 0; j<resultarray.length; j++){
+  if(resultarray[j] == (divisor / divident)){
+   	alreadyexists =true;
+	break;
+  }
 }
 
+if(!alreadyexists){
+  moduloiszero = true;
+}
+}
 
 }while(moduloiszero == false);
 
-solutionarray[i-1] = divisor / divident;
+resultarray[i-1] = divisor / divident;
+
 var li = document.getElementById("divisionli" + i);
 li.innerHTML = divisor + " / " + divident;
 
  }//End for loop
 }
 
-//Function for controlling the results
+/*====================================================================
+Function for controlling the results
+=====================================================================*/
 function controldivisiongame(){
 
 //Itearte through the Li elements
 var ul = document.getElementById("sortable");
 var items = ul.getElementsByTagName("li");
 for (var i = 0; i < items.length; ++i) {
-  alert(items[i].innerHTML);
+  alert(items[i].getAttribute("id"));
 }
 
 }
