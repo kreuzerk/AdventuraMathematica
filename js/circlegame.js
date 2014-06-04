56b68ca75f2bdf3c076eb33d488d96f44a507ce4
Kevin Kreuzer <kreuzerk@localhost.localdomain>
2014-06-04 22:30:25 +0200

    Initialisation of Angulo Game done

---
+++ js/circlegame.js
@@ -0,0 +1,37 @@
+var corners_data = [90, 45, 180, 200, 220, 10, 20, 50, 60, 75, 90, 15, 25, 90, 180, 45];
+//45, 90, 180 occur multiple times because they are more important than oder corners
+var used_corner_date = new Array(4); //In this array we will store all the Numbers that are already used
+
+//Important to make sure that each one is only used once
+
+function initialisecirclegame(){
+
+var exists;
+
+for(var i=0; i < used_corner_date.length; i++){
+do{
+
+exists = false;
+
+var randomnumber = Math.floor(Math.random() * corners_data.length);
+
+//Second for loop that ensure that each value is only used once
+for(var j = 0; j< used_corner_date.length; j++){
+	if(used_corner_date[j] == corners_data[randomnumber]){
+		exists = true;
+		break;	
+	}
+
+}//End second for
+
+if(exists == false){
+used_corner_date[i] = corners_data[randomnumber];
+var kendocircle = document.getElementById("circleh1_" + (i+1));
+kendocircle.innerHTML = corners_data[randomnumber];
+alert(kendocircle.innerHTML);
+}
+
+}while(exists == true);
+}//End first for
+
+}
